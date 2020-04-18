f2c0f463108aeb79f2b47c2ee8d020e4ece18011
Giovani Garfias <46204471+survani@users.noreply.github.com>
2020-04-17 18:25:28 -0700

    Added knexfile and its config file

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+//I have updated this file to how I want it to be like
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data.projects.db3",
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+};
