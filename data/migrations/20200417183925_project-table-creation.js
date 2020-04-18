7770a44722408a0b567d87eca5d931552e4ecdf2
Giovani Garfias <46204471+survani@users.noreply.github.com>
2020-04-17 19:02:46 -0700

    Added my last foriegn key connection table. I still need to add my down function

--- data/migrations/20200417183925_project-table-creation.js
+++ data/migrations/20200417183925_project-table-creation.js
@@ -1,32 +1,54 @@
 exports.up = function (knex) {
-  return knex.schema
-    .createTable("project", (tbl) => {
-      tbl.increments();
-      tbl.string("project_name", 128).unique().notNullable();
-      tbl.string("project_description", 128);
-      tbl.boolean("project_completed").notNullable().defaultTo(false);
-    })
-    .createTable("resource", (tbl) => {
-      tbl.increments();
-      tbl.string("resource_name", 128).unique().notNullable();
-      tbl.string("description", 128);
-    })
-    .createTable("task", (tbl) => {
-      tbl.increments();
-      tbl.string("task_description", 128).notNullable();
-      tbl.string("task_notes", 128);
-      tbl.boolean("task_completed").notNullable().defaultTo(false);
-      //This will be one of my connection. "Foreign Key"
-      //Will test this out, but I belive this should be good.
-      tbl
-        .integer("project_id")
-        .unsigned()
-        .notNullable()
-        .references("id")
-        .inTable("project")
-        .onUpdate("CASCADE")
-        .onDelete("CASCADE");
-    });
+  return (
+    knex.schema
+      .createTable("project", (tbl) => {
+        tbl.increments();
+        tbl.string("project_name", 128).unique().notNullable();
+        tbl.string("project_description", 128);
+        tbl.boolean("project_completed").notNullable().defaultTo(false);
+      })
+      .createTable("resource", (tbl) => {
+        tbl.increments();
+        tbl.string("resource_name", 128).unique().notNullable();
+        tbl.string("description", 128);
+      })
+      .createTable("task", (tbl) => {
+        tbl.increments();
+        tbl.string("task_description", 128).notNullable();
+        tbl.string("task_notes", 128);
+        tbl.boolean("task_completed").notNullable().defaultTo(false);
+        //This will be one of my connection. "Foreign Key"
+        //Will test this out, but I belive this should be good.
+        tbl
+          .integer("project_id")
+          .unsigned()
+          .notNullable()
+          .references("id")
+          .inTable("project")
+          .onUpdate("CASCADE")
+          .onDelete("CASCADE");
+      })
+      //important connection ("Foreign Key")
+      .createTable("project_resource", (tbl) => {
+        tbl.increments();
+        tbl
+          .integer("project_id")
+          .unsigned()
+          .notNullable()
+          .references("id")
+          .inTable("project")
+          .onUpdate("CASCADE")
+          .onDelete("CASCADE");
+        tbl
+          .integer("resource_id")
+          .unsigned()
+          .notNullable()
+          .references("id")
+          .inTable("task")
+          .onUpdate("CASCADE")
+          .onDelete("CASCADE");
+      })
+  );
 };
 
 exports.down = function (knex) {
